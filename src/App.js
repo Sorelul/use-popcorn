141f780e509ce6bed8e8e5558d1a77464ccd0e23
Sorelul <sorinake_eu@yahoo.com>
2025-01-23 18:05:49 +0200

    Pass movie data to components for improved state management

--- src/App.js
+++ src/App.js
@@ -45,20 +45,22 @@ const tempWatchedData = [
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+    const [movies, setMovies] = useState(tempMovieData);
+
     return (
         <>
-            <Navbar />
-            <MainContent />
+            <Navbar movies={movies} />
+            <MainContent movies={movies} />
         </>
     );
 }
 
-function Navbar() {
+function Navbar({ movies }) {
     return (
         <nav className="nav-bar">
             <Logo />
             <SearchBar />
-            <NumResults />
+            <NumResults movies={movies} />
         </nav>
     );
 }
@@ -72,10 +74,10 @@ function Logo() {
     );
 }
 
-function NumResults() {
+function NumResults({ movies }) {
     return (
         <p className="num-results">
-            Found <strong>X</strong> results
+            Found <strong>{movies.length}</strong> results
         </p>
     );
 }
@@ -94,29 +96,28 @@ function SearchBar() {
     );
 }
 
-function MainContent() {
+function MainContent({ movies }) {
     return (
         <main className="main">
-            <ListBox />
+            <ListBox movies={movies} />
             <WatchedBox />
         </main>
     );
 }
 
-function ListBox() {
+function ListBox({ movies }) {
     const [isOpen1, setIsOpen1] = useState(true);
     return (
         <div className="box">
             <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
                 {isOpen1 ? "–" : "+"}
             </button>
-            {isOpen1 && <MovieList />}
+            {isOpen1 && <MovieList movies={movies} />}
         </div>
     );
 }
 
-function MovieList() {
-    const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
     return (
         <ul className="list">
             {movies?.map((movie) => (
