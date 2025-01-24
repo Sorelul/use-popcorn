a223e61ef74525a36176d92a20364e2b039e955d
Sorelul <sorinake_eu@yahoo.com>
2025-01-24 18:37:07 +0200

    Add StarRating component for user feedback

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+        <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing 😎"]} />
     </React.StrictMode>
 );
