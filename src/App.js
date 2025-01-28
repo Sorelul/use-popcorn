2853598280856d338f0ac8fbe2c0dfc05750e298
Sorelul <sorinake_eu@yahoo.com>
2025-01-28 20:31:42 +0200

    Refactor fetchMovies function to use useEffect for improved data fetching and error handling

--- src/App.js
+++ src/App.js
@@ -53,32 +53,32 @@ export default function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
 
-    async function fetchMovies() {
-        try {
-            setIsLoading(true);
-            setError("");
-            const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`);
-
-            if (!res.ok) {
-                throw new Error("Something went wrong with fetching movies.");
-            }
-
-            const data = await res.json();
-
-            if (data.Response === "False") {
-                throw new Error("Movie not found.");
-            }
-
-            setMovies(data.Search);
-        } catch (err) {
-            console.error(err.message);
-            setError(err.message);
-        } finally {
-            setIsLoading(false);
-        }
-    }
-
     useEffect(() => {
+        async function fetchMovies() {
+            try {
+                setIsLoading(true);
+                setError("");
+                const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`);
+
+                if (!res.ok) {
+                    throw new Error("Something went wrong with fetching movies.");
+                }
+
+                const data = await res.json();
+
+                if (data.Response === "False") {
+                    throw new Error("Movie not found.");
+                }
+
+                setMovies(data.Search);
+            } catch (err) {
+                console.error(err.message);
+                setError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
         if (query.length < 3) {
             setMovies([]);
             setError("");
